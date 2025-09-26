4ae50580d1a775cf676390bff291bf7bb835ff69
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-09-26 22:56:33 +0300

    updata in navbar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
-
 export default function HomeLayout() {
   return (
     <>
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,34 +1,62 @@
-import React from 'react'
-import "./Navbar.css"
-import { NavLink } from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import WishList from './../../pages/WishList/WishList';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaHeart, FaShoppingCart, FaBoxOpen } from "react-icons/fa";
+import { FiLogIn, FiUserPlus, FiLogOut } from "react-icons/fi";
+import "./Navbar.css";
+
 export default function Navbar() {
   return (
-    <>
-      <header className="navbar" role="banner">
-    <Link href="#" className="brand" aria-label="الرئيسية">
-      <div className="logo">E</div>
-      E_commerce
-    </Link>
+    <header className="navbar" role="banner">
+      {/* Left: Logo + Brand */}
+      <Link to="/" className="brand">
+        <div className="logo">E</div>
+        E_Commerce
+      </Link>
 
-    <input className="hamburger" id="nav-toggle" type="checkbox" />
-    <label className="hamburger-label" for="nav-toggle" aria-hidden="true" title="فتح القائمة">
-      <span></span>
-    </label>
+      {/* Hamburger menu for mobile */}
+      <input className="hamburger" id="nav-toggle" type="checkbox" />
+      <label className="hamburger-label" htmlFor="nav-toggle">
+        <span></span>
+      </label>
 
-    <nav role="navigation" aria-label=" HomePage">
-      <ul className="menu">
-        <li><NavLink to="">Home</NavLink></li>
-        <li><NavLink to="ProductDetails">ProductDetails</NavLink></li>
-        <li><NavLink to="MyOrders">MyOrders</NavLink></li>
-        <li><NavLink to="WishList">WishList</NavLink></li>
-        <li><NavLink to="register" className="cta">Register</NavLink></li>
-        <li><NavLink to="Login" className="cta">Login</NavLink></li>
-      </ul>
-    </nav>
-  </header>
-
-    </>
-  )
+      {/* Right: Links */}
+      <nav role="navigation">
+        <ul className="menu">
+          <li>
+            <NavLink to="">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="ProductDetails">
+              <FaShoppingCart /> ProductDetails
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="MyOrders">
+              <FaBoxOpen /> MyOrders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="WishList">
+              <FaHeart /> WishList
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="register" className="cta">
+              <FiUserPlus /> Register
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="Login" className="cta">
+              <FiLogIn /> Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="logout" className="logout-btn">
+              <FiLogOut /> Logout
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
