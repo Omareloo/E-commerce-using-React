8dccd2eaad6de14e18c527e0a5f4863fbf610249
Omar Ali <omarh9392@gmail.com>
2025-10-01 23:16:31 +0300

    Revert "Eslam"

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
-import Slider from "react-slick";
 import slid1 from "../../assets/images/slider-image-1.jpeg";
 import slid2 from "../../assets/images/slider-image-2.jpeg";
 import slid3 from "../../assets/images/slider-image-3.jpeg";
 
+
+
+import Slider from "react-slick";
+
+
 import "./home.css";
 import Bar from "../../components/Bar/Bar";
-import { getCategories } from "../../services/categoryservice";
-import { getProducts } from "../../services/productservice";
-import Card from "../../components/Card/card";
+
 export default function Home() {
   const items = [slid1, slid2, slid3];
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
 
   const settings = {
     infinite: true,
@@ -21,89 +19,26 @@ export default function Home() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    arrows: false,
+    autoplaySpeed: 2000, // كل 2 ثانية
+    dots: true, // النقط تحت السلايدر
+    arrows: false, // يخفي الأسهم (ممكن تخليها true لو عايز)
   };
-useEffect(() => {
-  const fetchCats = async () => {
-    const res = await getCategories();
-    console.log("Categories response:", res); 
-    if (res && res.success) {
-      setCategories(res.data);
-    }
-   
-    else if (Array.isArray(res)) {
-      setCategories(res);
-    }
-  };
-  fetchCats();
-}, []);
-
-
-  // 2. هات كل المنتجات
- useEffect(() => {
-  const fetchProducts = async () => {
-    const res = await getProducts();
-    console.log("Products response:", res); // Debug
-
-    // لو الـ service بيرجع object فيه success + data
-    if (res && res.success) {
-      setProducts(res.data);
-      setFilteredProducts(res.data);
-    }
-    // لو بيرجع Array على طول
-    else if (Array.isArray(res)) {
-      setProducts(res);
-      setFilteredProducts(res);
-    }
-  };
-  fetchProducts();
-}, []);
-
-
-  // 3. الفلترة حسب الكاتيجوري
-const handleSelectCategory = (catId) => {
-  if (catId === "all") {
-    setFilteredProducts(products);
-  } else {
-    const filtered = products.filter((p) => p.Category?._id === catId);
-    setFilteredProducts(filtered);
-  }
-};
-
-
 
   return (
-    <>
-      <div className="imageItems">
-        <Slider {...settings}>
-          {items.map((item, index) => (
-            <div key={index}>
-              <img src={item} alt={`slide-${index}`} className="imgSlider" />
-            </div>
-          ))}
-        </Slider>
-      </div>
-
-      {/* Bar */}
-   
-      <Bar
-  categories={categories}   
-  onSelect={handleSelectCategory}
-/>
-  <div className="products">
-      {filteredProducts.length > 0 ? (
-        filteredProducts.map((p) => (
-          <Card
-            key={p._id}
-            product={p}
-          />
-        ))
-      ) : (
-        <p>No products available</p>
-      )}
+ <>
+    <div className="imageItems">
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <div key={index}>
+            <img src={item} alt={`slide-${index}`} className="imgSlider" />
+          </div>
+        ))}
+      </Slider>
     </div>
-    </>
+    <Bar 
+  categories={["Clothes", "Electronics", "Furniture"]} 
+  onSelect={(cat) => console.log(cat)} 
+/>
+ </>
   );
 }
