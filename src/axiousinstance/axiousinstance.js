be371b2991195684417f0fae61cdfa4422cbd54b
ShenoudaDaniel07 <shenoudadaniel777@gmail.com>
2025-10-01 19:40:55 +0200

    Update axiousinstance.js

--- src/axiousinstance/axiousinstance.js
+++ src/axiousinstance/axiousinstance.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: 'http://localhost:3000/api/v1',
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Admin ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
