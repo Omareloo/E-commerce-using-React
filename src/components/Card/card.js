12d84e44d8e6fa6490f2bf53ef3ca2912c2634eb
Omar Ali <omarh9392@gmail.com>
2025-10-02 19:40:35 +0300

    Revert "Revert 9 eslam"

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,5 +1,28 @@
-function Card(props) {
-  return <div>Card Component</div>;
+import React from "react";
+import "./card.css"
+export default function Card({ product}) {
+  return (
+    <div className="card">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="card-img"
+      />
+      <div className="card-body">
+        <h3 className="card-title">
+          {product.title.split(" ").slice(0, 3).join(" ")}
+        </h3>
+        <p className="card-price">{product.price} EGP</p>
+        <p className="card-category">{product.Category?.name}</p>
+      </div>
+      <div className="card-footer">
+        <button
+          className="btnAdd"
+        
+        >
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default Card;
