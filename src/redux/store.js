347ddc94bc7c12e56315701596fd449de7688eee
Aya Hussien <88637052+ayahussienmohamed@users.noreply.github.com>
2025-10-04 17:51:10 +0300

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
-import wishlistReducer from './wishlistSlice';
-import createOrderReducer from './createOrderSlice';
+import cartReducer from './slices/cartSlice';
+import wishlistReducer from './slices/wishlistSlice';
+import createOrderReducer from './slices/createOrderSlice';
 
 const store = configureStore({
   reducer: {
